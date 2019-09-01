4ed282633c952fa1a2d2571fd80ef6d212b0fdd1
高桥老湿 <gaoqiaolaoshi@qq.com>
2019-09-01 15:50:38 +0800

    项目初始化

---
+++ pages/tab.vue
@@ -0,0 +1,67 @@
+<template>
+  <div style="padding:50px,margin-bottom:20px">
+    <div class="tab">
+        <span 
+        v-for="(item,index) in ['新闻','体育','娱乐']" :key="index" 
+        @mousemove="ontab(index)"
+        :class="{active:current===index}"
+        >{{item}}</span>
+    </div>
+
+    <div v-show="current === 0">
+      <ul>
+        <li>中秋将至</li>
+        <li>中秋将至</li>
+        <li>中秋将至</li>
+      </ul>
+    </div>
+
+    <div v-show="current === 1">
+      <ul>
+        <li>中国男篮</li>
+        <li>中国男篮</li>
+        <li>中国男篮</li>
+      </ul>
+    </div>
+
+    <div v-show="current === 2">
+      <ul>
+        <li>文章离婚</li>
+        <li>文章离婚</li>
+        <li>文章离婚</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            current:0
+        }
+    },
+    // mounted-钩子
+    // methods-方法
+    methods:{
+        // 点击事件
+        ontab(index){
+            // 效果返回值
+            this.current = index
+        }
+    }
+}
+</script>
+
+<style lang='less' scoped>
+.tab{
+    margin-bottom: 35px;
+    span{
+        // 鼠标手
+        cursor: pointer;
+    }
+}
+.active{
+    color: skyblue;
+}
+</style>
